85d658ea88a937139ee40249aa9b76c7e63bfab3
mattespositotech <matt@teamduality.dev>
2024-03-08 15:15:21 -0600

    Added Frontend Validation

--- scripts/signIn.js
+++ scripts/signIn.js
@@ -5,6 +5,7 @@ const signInHTML = `
     <h2 class="sign-in-title">Sign Into Your Account</h2>
     <p class="sign-in-text">Your account stores all your reindeer roundup information</p>
     <form id="signInForm">
+        <div id="validationMessage" class="form-error"></div>
         <input class="form-input" type="text" id="sign-in-email" name="sign-in-email" placeholder="Enter your email"><br>
         <input class="form-input" type="password" id="sign-in-password" name="sign-in-password" placeholder="Enter your password"><br>
         <button class="bg-gn btn-sign-in" type="button" onclick="signInButton(event)">Sign In</button>
@@ -21,6 +22,7 @@ const createAccountHTML = `
     <h2 class="sign-in-title">Create an Account</h2>
     <p class="sign-in-text">Create an account to store all your reindeer roundup information</p>
     <form id="createAccountForm">
+        <div id="validationMessage" class="form-error"></div>
         <input class="form-input" type="text" id="create-email" name="create-email" placeholder="Enter your email"><br>
         <input class="form-input" type="password" id="create-password" name="create-password" placeholder="Enter your password"><br>
         <input class="form-input" type="password" id="confirm-password" name="confirm-password" placeholder="Confirm your password"><br>
@@ -34,6 +36,7 @@ const forgotPasswordHTML = `
     <h2 class="sign-in-title">Forgot your Password?</h2>
     <p class="sign-in-text">Enter your email and we will send you a new temporary password</p>
     <form id="forgotPasswordForm">
+        <div id="validationMessage" class="form-error"></div>
         <input class="form-input" type="text" id="forgot-email" name="forgot-email" placeholder="Enter your email"><br>
         <button class="bg-gn btn-sign-in" type="submit" onclick="forgotPasswordButton(event)">Send Email</button>
     </form>
@@ -69,10 +72,43 @@ function createAccount() {
     display('createAcc')
 }
 
-function validateEmail(email) {
+function invalidEmail(email) {
     var emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
 
-    return emailPattern.test(email)
+    return !emailPattern.test(email)
+}
+
+function validatePassword(password) {
+    const errors = [];
+
+    if (password.length < 6) {
+        errors.push("Your password must be at least 6 characters.");
+    }
+    if (password.length > 128) {
+        errors.push("Your password must be less than 129 characters.");
+    }
+    if (!/[A-Z]/.test(password)) {
+        errors.push("Your password must contain at least one uppercase letter.");
+    }
+    if (!/[a-z]/.test(password)) {
+        errors.push("Your password must contain at least one lowercase letter.");
+    }
+    if (!/[0-9]/.test(password)) {
+        errors.push("Your password must contain at least one number.");
+    }
+    if (!/[\W_]/.test(password)) {
+        errors.push("Your password must contain at least one special character.");
+    }
+
+    return errors;
+}
+
+function displayUserError(message) {
+    document.getElementById('validationMessage').innerText = message
+}
+
+function clearError() {
+    displayUserError("")
 }
 
 function signInButton(e) {
@@ -80,6 +116,13 @@ function signInButton(e) {
 
     var email = document.getElementById('sign-in-email').value;
     var password = document.getElementById('sign-in-password').value;
+
+    if (invalidEmail(email)) {
+        displayUserError('Not a valid email.')
+        return
+    }
+
+    clearError()
 
     console.log(email, password);
 }
@@ -91,9 +134,24 @@ function createAccountButton(e) {
     var password = document.getElementById('create-password').value;
     var confirmPassword = document.getElementById('confirm-password').value;
 
+    let errors = []
+
+    if (invalidEmail(email)) {
+        errors.push('Not a valid email.')
+    } 
     if (password !== confirmPassword) {
-        console.log('Invalid password')
+        errors.push("Passwords don't match.")
     }
+
+    errors.push(...validatePassword(password))
+
+    if (errors.length !== 0) {
+        const errorString = errors.join('\n')
+        displayUserError(errorString)
+        return
+    }
+    
+    clearError()
 
     console.log(email, password, confirmPassword)
 }
@@ -103,7 +161,12 @@ function forgotPasswordButton(e) {
 
     var email = document.getElementById('forgot-email').value;
 
-    console.log(validateEmail(email))
+    if (invalidEmail(email)) {
+        displayUserError('Not a valid email')
+        return
+    }
+
+    clearError()
 }
 
 function signInRedirect() {
